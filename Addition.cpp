a3beeb6fe50c7507f493d9281249ef6e43e1b0da
Janindu <dhananjayajanindu@gmail.com>
2025-03-06 15:23:29 +0530

    Add multiply function to the script

--- Addition.cpp
+++ Addition.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b) {
-    return a + b;
+// Function to perform addition
+double add(double a, double b) {
+	return a + b;
+}
+
+// Function to perform multiplication
+double multiply(double a, double b) {
+	return a * b;
 }
 
 int main() {
-    int num1, num2;
-    
-    //Ask user to enter first number 	
-    cout << "Enter first number: ";
-    cin >> num1;
+	double num1, num2;
 
-    //Ask user to enter second number 
-    cout << "Enter second number: ";
-    cin >> num2;
+	// Get user input
+	cout << "Enter first number: ";
+	cin >> num1;
 
-    //Assign add function value to result varieble
-    int result = add(num1, num2);
+	cout << "Enter second number: ";
+	cin >> num2;
 
-    //Display output
-    cout << "The sum of " << num1 << " and " << num2 << " is: " << result << endl;
+	// Perform addition and multiplication
+	double result_add = add(num1, num2);
+	double result_multiply = multiply(num1, num2);
 
-    return 0;
+	// Display the results
+	cout << "The sum of " << num1 << " and " << num2 << " is: " << result_add << endl;
+	cout << "The product of " << num1 << " and " << num2 << " is: " << result_multiply << endl;
+
+	return 0;
 }
